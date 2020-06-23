b2b3ef5136b7a0ca362addf8b9aadac43bc5a677
andychod <andysuper110774@gmail.com>
2020-06-23 22:42:44 +0800

    add tang_poetry

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainsite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tangpoetry/', views.tang_poetry),
+    path('tangpoetry/post/<slug:slug>', views.tang_poetry_showpost),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from datetime import datetime
+from mainsite import models
 
-# Create your views here.
+
+# 唐詩網頁用
+def tang_poetry(request):
+    posts = models.Post.objects.all()
+    now = datetime.now()
+    return render(request, 'tang_poetry/tang_poetry_index.html', locals())
+
+def tang_poetry_showpost(request, slug):
+    print(slug)
+    try:
+        post = models.Post.objects.get(slug = slug)
+        if post != None:
+            return render(request, 'tang_poetry/tang_poetry_post.html', locals())
+    except:
+        return redirect('/')
